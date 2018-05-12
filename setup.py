ceea9f40000f02f14c413c3f71084e52de55ce52
Daniel Probst <daenuprobst@gmail.com>
2018-05-12 08:34:09 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='faerun-python',
+      version='1.0.0',
+      descrition='A python module for generating interactive views of chemical spaces.',
+      author='Daniel Probst',
+      author_email='daenuprobst@gmail.com',
+      url='')
