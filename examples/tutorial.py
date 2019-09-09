73270954a0be465df96fcfeb65d0577620dd40fa
Daniel Probst <daenuprobst@gmail.com>
2019-09-09 16:01:58 +0200

    ...

--- examples/tutorial.py
+++ examples/tutorial.py
@@ -4,18 +4,16 @@ from faerun import Faerun
 
 
 def main():
-    f = Faerun(
-        clear_color="#222222",
-        coords=True,
-        view="front",
-    )
+    f = Faerun(clear_color="#222222", coords=True, view="front")
 
     x = np.linspace(0, 12.0, 326)
     y = np.sin(np.pi * x)
     z = np.cos(np.pi * x)
     c = np.random.randint(0, 2, len(x))
 
-    data = {"x": x, "y": y, "z": z, "c": c, "labels": c}
+    labels = [str(l) + "__Test" for l in c]
+
+    data = {"x": x, "y": y, "z": z, "c": c, "labels": labels}
 
     f.add_scatter(
         "helix",
@@ -26,6 +24,10 @@ def main():
         categorical=True,
         has_legend=True,
         legend_labels=[(0, "Zero"), (1, "One")],
+    )
+
+    f.add_tree(
+        "helixtree", {"from": [1, 5, 6, 7], "to": [2, 7, 8, 9]}, point_helper="helix"
     )
 
     f.plot("helix")
